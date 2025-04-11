e79d90e793369a4f6fe512188d203708eaf7f462
ntjennings1 <ntjennings1@gmail.com>
2025-04-10 20:52:19 -0400

    Doc Standard 1.13

--- scripts/bash/state/reboot.sh
+++ scripts/bash/state/reboot.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+# Reboots the machine
+#
+# @return null
 function reboot(){
     sudo reboot 
 }
 
+# Runs the reboot function
 reboot
--- scripts/bash/state/update.sh
+++ scripts/bash/state/update.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+# Updates the machine
+#
+# @return null
 function update(){
     sudo apt-get update -y
 }
 
+# Runs the update function
 update
